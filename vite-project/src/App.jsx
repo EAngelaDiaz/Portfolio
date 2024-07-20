c9c9dc33ba61c9165fb63e03a1d00c818463fb4d
estrella <angelaestrelladiaz@outlook.com>
2024-07-19 23:08:55 -0300

    vite-tailwind

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function App() {
+  
+  return (
+    <div className="w-64 mx-auto bg-secondary text-black shadow-lg rounded-lg overflow-hidden h-64">
+      <h1 className="text-lg text-center font-semibold truncate">Hello Word</h1>
+    </div>
+  )
+}
+
+export default App
